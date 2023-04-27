a81eabfae2fd482c033a95ad7686b62d26639108
hrishikesh147 <hrishikeshbhagawati@gmail.com>
2023-04-27 15:15:37 +0530

    model trainer

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,27 +18,26 @@ from src.exception import CustomException
 from src.logger import logging
 
 from src.utils import save_object,evaluate_models
-from sklearn.metrics import r2_score
 
 @dataclass
-class ModelTrainerConfig():
+class ModelTrainerConfig:
     trained_model_file_path=os.path.join("artifacts","model.pkl")
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
+
     def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("splitting training and test data input")
+            logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-
-            models={
+            models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
@@ -47,7 +46,6 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -87,19 +85,21 @@ class ModelTrainer:
             }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                            models=models,param=params)
+                                             models=models,param=params)
             
-            best_model_score=max(sorted(model_report.values()))
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
 
             best_model_name = list(model_report.keys())[
-                    list(model_report.values()).index(best_model_score)
-                ]
-            
+                list(model_report.values()).index(best_model_score)
+            ]
             best_model = models[best_model_name]
 
             if best_model_score<0.6:
-                raise CustomException("no best model found")
-            logging.info(f"best model found on training and testing dataset")
+                raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
@@ -110,7 +110,10 @@ class ModelTrainer:
 
             r2_square = r2_score(y_test, predicted)
             return r2_square
+            
 
 
+
+            
         except Exception as e:
             raise CustomException(e,sys)
